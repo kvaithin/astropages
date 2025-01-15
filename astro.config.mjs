3fde02fe490da466957c39097a02835bb9db0c0e
Karthik Vaithin <karthik.vaithin@aexp.com>
2025-01-15 20:51:53 +0000

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kvaithin.github.io',
+  base: '/astropages',
+});
